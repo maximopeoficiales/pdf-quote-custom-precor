536250b36633b4cf761f3dca8b3d77fdaabfeee2
Maximo Junior Apaza Chirhuana <maximopeoficiales@gmail.com>
2021-02-10 00:31:34 -0500

    se acabo diseño falta contenido de la tabla

--- pdf/quote.php
+++ pdf/quote.php
@@ -49,6 +49,10 @@ $logo = $logo_attachment_id ? get_attached_file($logo_attachment_id) : $logo_url
 		.text-8 {
 			font-size: 8px !important;
 		}
+
+		.text-9 {
+			font-size: 9px !important;
+		}
 	</style>
 </head>
 
@@ -56,9 +60,9 @@ $logo = $logo_attachment_id ? get_attached_file($logo_attachment_id) : $logo_url
 	<div>
 		<div class="p-2">
 			<img src="<?php echo esc_url($logo); ?>" class="rounded-lg" style="max-width: 150px;">
-			<div class="text-uppercase float-right">
-				<p class="border p-2 border-dark my-1"><b>Cotizacion:</b> <?= $order_id ?></p>
-				<p class="border p-2 border-dark my-1"><b>Fecha:</b> <?= $data["date_created"] ?></p>
+			<div class="border text-uppercase float-right mb-2">
+				<p class=" p-2  my-1"><b>Cotizacion:</b> <?= $order_id ?></p>
+				<p class=" p-2  my-1"><b>Fecha:</b> <?= $data["date_created"] ?></p>
 			</div>
 		</div>
 
@@ -88,32 +92,194 @@ $logo = $logo_attachment_id ? get_attached_file($logo_attachment_id) : $logo_url
 			</tr>
 		</thead>
 		<tbody class="text-8">
-			<tr>
-				<td scope="row" class="text-8 text-center">0010</td>
-				<td class="text-8 text-center">336.000</td>
-				<td class="text-8 text-center">403036</td>
-				<td class="text-8 text-left">PERFIL PARANTE 64 X 38 X 0.45 X 3 GALV</td>
-				<td class="text-8 text-center">PAQ</td>
-				<td class="text-8 text-right">2,688</td>
-				<td class="text-8 text-right">12.63</td>
-				<td class="text-8 text-right">2.49</td>
-				<td class="text-8 text-right">1.57</td>
-				<td class="text-8 text-right">4,216.66</td>
-				<td class="text-8 text-right">4,244.35</td>
-			</tr>
+			<?php
+			// recorro los productos
+			$cont = 0;
+			foreach ($data["line_items"] as $product) {
+				$cont += 10;
+			?>
+				<tr>
+					<td scope="row" class="text-8 text-center"><?php echo str_pad($cont, 4, "0", STR_PAD_LEFT); ?></td>
+					<td class="text-8 text-center"><?= $product["quantity"] ?></td>
+					<td class="text-8 text-center"><?= $product["sku"] ?></td>
+					<td class="text-8 text-left"><?= $product["name"] ?></td>
+					<td class="text-8 text-center">PAQ</td>
+					<td class="text-8 text-right">2,688</td>
+					<td class="text-8 text-right">12.63</td>
+					<td class="text-8 text-right">2.49</td>
+					<td class="text-8 text-right">1.57</td>
+					<td class="text-8 text-right"><?= $product["subtotal"] ?></td>
+					<td class="text-8 text-right">4,244.35</td>
+				</tr>
+			<?php
+			} ?>
 		</tbody>
 	</table>
+
 	<div class="p-2 my-2">
-		<h6 class="text-bold">Observaciones</h6>
-		<div class="text-uppercase float-right">
-			<p class="border p-2 border-dark my-1"><b>PESO TOT KG:</b> 13,874.69</p>
-			<p class="border p-2 border-dark my-1"><b>Subtotal:</b> 13,997.49</p>
-			<p class="border p-2 border-dark my-1"><b>IGV (18%):</b> 2,519.55</p>
-			<p class="border p-2 border-dark my-1"><b>PERC.( %):</b> 0.00</p>
-			<p class="border p-2 border-dark my-1"><b>TOTAL USD:</b> 16,517.04</p>
+		<!-- ejemplo de simulacion de flex -->
+		<h6 class="text-bold">Observaciones:</h6>
+		<div class="border border-dark text-uppercase float-right">
+			<p class=" p-2  "><b>PESO TOT KG:</b> 13,874.69</p>
+			<p class=" p-2 "><b>Subtotal:</b> <?= round($currentOrder->get_total() / 1.18, 2) ?></p>
+			<p class=" p-2  "><b>IGV (18%):</b> <?= round($currentOrder->get_total() / 0.18, 2) ?></p>
+			<p class=" p-2 "><b>PERC.( %):</b> 0.00</p>
+			<p class=" p-2  "><b>TOTAL USD:</b> <?= $currentOrder->get_total() ?></p>
+		</div>
+		<div class="">
+			<div style="width: 70%;">
+				<p class="text-10">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+				<p>
+			</div>
+
+		</div>
+
+	</div>
+	<br><br>
+	<br><br><br><br>
+	<!-- inicio de condiciones -->
+	<div class="my-2">
+		<h6 class="text-bold text-10">I. CONDICIONES COMERCIALES</h6>
+		<div class="p-2">
+			<table class="table table-bordered text-9 table-sm">
+				<thead class="d-none">
+					<tr>
+						<th style="width: 30%;">Condicion</th>
+						<th style="width: 70%;">Descripcion</th>
+					</tr>
+				</thead>
+				<tbody class="text-left">
+					<tr>
+						<td><b>FORMA DE PAGO</b></td>
+						<td>Letra 30 días</td>
+					</tr>
+					<tr>
+						<td><b>VIAS DE PAGO</b></td>
+						<td>CUENTA RECAUDADORA: BCP, CONTINENTAL y SCOTIABANK,TARJETAS DE CREDITO</td>
+					</tr>
+
+					<tr>
+						<td><b>TIEMPO DE ENTREGA</b></td>
+						<td></td>
+					</tr>
+					<tr>
+						<td><b>VALIDEZ DE OFERTA</b></td>
+						<td>08 DIAS</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+
+		<div class="my-2 w-100">
+			<div class="py-2 pl-5">
+				<ol>
+					<li>
+						El Cliente deberá confirmar las medidas, cantidades, precios y la condición de entrega indicados en la presente cotización.
+					</li>
+					<li>
+						El pedido detallado en la cotización tendrá una vigencia de stock y precios de 24 horas una vez emitida. Posterior a ello, vía
+						comunicación con su Ejecutivo de Ventas, deberán confirmar la disponibilidad de stock y precio. <b>Nota:</b> Cualquier producto
+						no considerado en esta cotización será considerado como adicional y deberá solicitarse su cotización.
+					</li>
+					<li>
+						Los plazos de entrega serán contabilizados desde el Pago Íntegro del Pedido (para Venta al Contado) o desde el Pago del
+						Adelanto (para Venta al Crédito).
+					</li>
+					<li>
+						Todos los créditos otorgados por un plazo mayor a 30 días calendario se encuentran afectos al cobro de intereses.
+					</li>
+				</ol>
+			</div>
 		</div>
 	</div>
+	<div class="my-2">
+		<h6 class="text-bold text-10">II. CONDICIONES DE ENTREGA</h6>
+		<div class="p-2">
+			<p class="p-2 border text-center text-9"><b>DESPACHO/LOCAL DEL CLIENTE/DESESTIBA A CARGO DE PRECOR/COLOCAR EN RACKS</b></p>
+		</div>
+		<p class="my-1">Precor entrega el material en el lugar de destino acordado por el Cliente:</p>
+		<div class="my-2 w-100 text-9">
+			<div class="py-2 pl-5">
+				<ol>
+					<li>
+						PRECOR S.A. suministrará Los Materiales en el lugar previamente designado y especificado por el Cliente dentro de Lima
+						Metropolitana.
+					</li>
+					<li>
+						Se considerará que el servicio de entrega de Los Materiales ha concluido cuando se firma la Guía de Remisión con la
+						conformidad de la entrega. Luego de dicha entrega, PRECOR S.A. no es responsable de lo que suceda durante el
+						manipuleo interno de los Materiales por parte del Cliente.
+					</li>
+					<li>
+						De presentarse observaciones del producto con posterioridad a la entrega, el Cliente tendrá 15 días calendario para
+						presentar su reclamo, computados a partir del día de entrega, de no realizarse el reclamo dentro del plazo establecido se
+						entenderá que no existe ninguna observación y cualquier reclamo posterior no será aceptado. Realizado el reclamo
+						(según los términos antes descritos), el departamento de Post Venta se presentará en las instalaciones en las cuales se
+						encuentren Los Materiales dentro de las 24 horas para absolver consultas y realizar el informe respectivo.
+					</li>
+					<li>
+						Si por alguna razón el Cliente o agencia asignada no acepta que se descargue el material o excede el tiempo máximo de
+						espera de la unidad (01hora), por causas imputables al Cliente, el transporte regresará a PRECOR S.A. y no se realizará
+						un nuevo servicio de flete salvo que el Cliente acepte el presupuesto de transporte; de ser así, PRECOR S.A. emitirá una
+						factura por dicho servicio, de acuerdo al costo en el que incurra.
+					</li>
+				</ol>
+			</div>
+		</div>
+	</div>
+	<div class="my-2">
+		<h6 class="text-bold text-10">III. CONDICIONES GENERALES</h6>
+		<div class="my-2 w-100 text-9">
+			<div class="py-2 pl-5">
+				<ol>
+					<li>
+						Venta al Contado: PRECOR S.A. se reserva la propiedad del bien materia de esta cotización, hasta que se haya realizado
+						el pago íntegro del precio, dentro del plazo establecido y según las condiciones acordadas. La falta de pago del íntegro del
+						precio pactado, motivará la ejecución del pacto de reserva de propiedad, pudiendo PRECOR S.A. disponer del bien; lo
+						cual será puesto en conocimiento del cliente.
+					</li>
+					<li>
+						De no concretarse la venta, del monto entregado en calidad de adelanto, PRECOR S.A retendrá el monto correspondiente
+						a los costos y gastos incurridos tanto por el almacenamiento de Los Materiales, como por los gastos de fabricación
+						incurridos
+					</li>
+					<li>
+						Ante el incumplimiento de los pagos por parte del Cliente, en las fechas acordadas, PRECOR S.A. se encontrará facultado
+						a cobrar gastos financieros por concepto de intereses moratorios y compensatorios.
+					</li>
+					<li>
+						PMP HOLDING S.A. es una empresa respetuosa de la normativa anticorrupción vigente: Ley N° 30424, D.L. N° 1352,
+						D.L. N° 1385, siendo esta lista enunciativa y no taxativa. En mérito a ello, los Clientes y/o Proveedores declaran y
+						garantizan que han leído todas las políticas, procedimientos y Línea Ética (Sistema Confidencial de Denuncias) de
+						PMP HOLDING S.A., los cuales se encuentran disponibles en la web <a href="https://www.lineaeticapmp.com/" target="_blanks"> https://www.lineaeticapmp.com/</a> y que además
+						se alinean al cumplimiento de todos los dispositivos legales previamente enunciados. En tal sentido, PMP HOLDING
+						S.A. no asume ninguna responsabilidad por incumplimientos ni por ilícitos de sus Clientes y/o Proveedores, en materia de
+						corrupción y/o LAFT (Lavado de Activos y Financiamiento del Terrorismo).
+					</li>
+				</ol>
+			</div>
+		</div>
+	</div>
+	<!-- fin de condiciones -->
+	<div class="my-2 float-left">
+		<p>VIVIANA ALARCON AQUIJE</p>
+		<p>7054000 ext. 2102</p>
+		<p>Av. Manuel Olguin 373, Edificio Qubo</p>
+		<p>Piso 9, Surco, Lima</p>
+		<p>Telf.: (511) 705-4000</p>
+		<p>www.PRECOR.com.pe</p>
+	</div>
+	<div class="my-2 float-right">
+		<br>
+		<br>
+		<br>
+		<br>
+		<div class="border-top " style="border-color: black; width: 200px;">
+			<p class=""><b>Sello y firma del Aceptante</b></p>
+			<p>Nombre:</p>
+		</div>
 
+	</div>
 </body>
 
 </html>
