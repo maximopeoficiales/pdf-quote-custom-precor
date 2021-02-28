8bb4635d6adc4cbcfe9ca39b3764577cab6c585d
Maximo Junior Apaza Chirhuana <maximopeoficiales@gmail.com>
2021-02-27 20:58:30 -0500

    se agrego titulo al quote template pd

--- pdf/quote.php
+++ pdf/quote.php
@@ -33,6 +33,7 @@ $logo = $logo_attachment_id ? get_attached_file($logo_attachment_id) : $logo_url
 <html>
 
 <head>
+	<title><?php echo "Pedido " . $order_id; ?></title>
 	<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
 	<style type=" text/css">
